399d3766f2573cb41a6ee7e7a82c0b33aea2fa55
varioustoxins <garyt.and.sarahb@gmail.com>
2021-07-07 21:35:35 +0100

    add isolate_frame to test_lib

--- lib/test_lib.py
+++ lib/test_lib.py
@@ -1,5 +1,7 @@
 from itertools import zip_longest
+from typing import Optional
 
+from pynmrstar import Entry
 
 
 def assert_lines_match(expected: str, reported: str,  display:bool=False):
@@ -17,7 +19,27 @@ def assert_lines_match(expected: str, reported: str,  display:bool=False):
     for i,(expected_line, header_line) in enumerate(zip_lines):
         if display:
             print(f'exp|{i}|',expected_line.strip())
-            print(f'rep|{i}|',header_line.strip())
+            print(f'rep|{i}',header_line.strip())
             print()
 
         assert expected_line.strip() == header_line.strip()
+
+
+def isolate_frame(target: Entry, name: str) -> Optional[str]:
+    """
+    Extract one frame from a NEF file by name as a sting
+    Args:
+        target (Entry): target NEF entry
+        name (str): name of the save frame
+
+    Returns:
+        Optional[str]: the entry or a None if not found
+    """
+    entry = None
+    try:
+        entry = Entry.from_string(target.stdout)
+        entry = str(entry.get_saveframe_by_name(name))
+    except:
+        pass
+
+    return entry
