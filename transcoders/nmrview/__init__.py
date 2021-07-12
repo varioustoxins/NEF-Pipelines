e13b167db4400444a485e604456e948009aeb819
varioustoxins <garyt.and.sarahb@gmail.com>
2021-07-12 19:51:02 +0100

    only init nef_app if nef_app.app is not None (for tests)
    tests may need to load libraries in plugins when nef_app.app isn't used, ie the process is not a typer cli

--- transcoders/nmrview/__init__.py
+++ transcoders/nmrview/__init__.py
@@ -4,11 +4,12 @@ app = typer.Typer()
 import_app = typer.Typer()
 export_app = typer.Typer()
 
-nef_app.app.add_typer(app, name='nmrview', help='-  read and write nmrview [peaks, sequences & shifts]')
+if nef_app.app:
+    nef_app.app.add_typer(app, name='nmrview', help='-  read and write nmrview [peaks, sequences & shifts]')
 
-app.add_typer(import_app, name='import', help='-  import nmrview [peaks, sequences & shifts]')
-app.add_typer(export_app, name='export', help='-  export nmrview [peaks, sequences & shifts]')
+    app.add_typer(import_app, name='import', help='-  import nmrview [peaks, sequences & shifts]')
+    app.add_typer(export_app, name='export', help='-  export nmrview [peaks, sequences & shifts]')
 
-# import of specific importers must be after app creation to avoid circular imports
-import transcoders.nmrview.importers.sequence
-import transcoders.nmrview.importers.peaks
+    # import of specific importers must be after app creation to avoid circular imports
+    import transcoders.nmrview.importers.sequence
+    import transcoders.nmrview.importers.peaks
