b8fe4c9a3d75d4912a50311456f9c0e6a227f1c8
varioustoxins <garyt.and.sarahb@gmail.com>
2021-07-12 08:40:59 +0100

    new nmrview tcl parser parses basic quoted strings

--- tests/test_tcl.py
+++ tests/test_tcl.py
@@ -1,16 +1,27 @@
 import pytest
 
-from pyparsing import Word, alphanums
+from pyparsing import Word, alphanums, Suppress, ZeroOrMore
+DOUBLE_QUOTE = Suppress('"')
 
 @pytest.fixture
 def parser():
 
     word = Word(alphanums + '.')
+    word.setName('word')
+    quoted_list = (DOUBLE_QUOTE + ZeroOrMore(word) + DOUBLE_QUOTE)
+    quoted_list.setName('quoted list')
 
-    return word
+    phrase = word | quoted_list
+
+    phrase.setName("tcl")
 
 
-def test_basic_value(parser):
+    phrase.create_diagram('tcl_diag.html')
+
+    return phrase
+
+
+def test_basic_word(parser):
     values = [
         "123",
         "abc",
@@ -24,6 +35,32 @@ def test_basic_value(parser):
         assert parsed[0] == value
 
 
+def test_quoted_word(parser):
+    values = [
+        '"123"',
+        '"abc"',
+        '"123.abc"'
+    ]
+
+    for value in values:
+        parsed = parser.parseString(value)
+
+        assert len(parsed) == 1
+        assert parsed[0] == value.strip('"')
+
+
+def test_quoted_word_list(parser):
+    values = [
+        '"123 456"',
+        '"abc def"',
+        '"123.abc 456.def"'
+    ]
+
+    for value in values:
+        parsed = parser.parseString(value)
+
+        assert len(parsed) == 2
+        assert list(parsed) == value.strip('"').split()
 
 
 if __name__ == '__main__':
