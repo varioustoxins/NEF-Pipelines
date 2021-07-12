25dc8e3e6a87410ccad19614c889f41f0fb766a0
varioustoxins <garyt.and.sarahb@gmail.com>
2021-07-12 18:44:57 +0100

    allow groups of quoted strings

--- tests/test_tcl.py
+++ tests/test_tcl.py
@@ -1,20 +1,50 @@
 import pytest
 
-from pyparsing import Word, alphanums, Suppress, ZeroOrMore
+from pyparsing import Word, alphanums, Suppress, ZeroOrMore, Group, Forward, alphas, nums
+
 DOUBLE_QUOTE = Suppress('"')
+
+# TODO is this a hack if so how to do this
+def process_emptys_and_singles(value):
+
+    for i, item in enumerate(value):
+        if len(item) == 0:
+            value[i] = ""
+
+    if len(value) == 1:
+        value = value[0]
+
+    return value
+
 
 @pytest.fixture
 def parser():
 
-    word = Word(alphanums + '.')
-    word.setName('word')
-    quoted_list = (DOUBLE_QUOTE + ZeroOrMore(word) + DOUBLE_QUOTE)
-    quoted_list.setName('quoted list')
+    simple_word = Word(alphanums + '.')
+    simple_word.setName('simple_word')
 
-    phrase = word | quoted_list
+    expression = Forward()
+    expression.setName('expression')
 
-    phrase.setName("tcl")
 
+    DBL_QUOTE = Suppress('"')
+    LEFT_PAREN = Suppress("{")
+    RIGHT_PAREN = Suppress("}")
+
+    quoted_simple_word = DBL_QUOTE + simple_word + DBL_QUOTE
+    quoted_simple_word.setName('quoted_simple_word')
+
+    quoted_complex_word = Group(DBL_QUOTE + ZeroOrMore(expression) + DBL_QUOTE)
+    quoted_complex_word.setName('quoted complex word')
+
+    complex_list = Group(LEFT_PAREN + ZeroOrMore(expression) + RIGHT_PAREN)
+    complex_list.setName('complex list')
+
+    expression << (simple_word | quoted_simple_word | quoted_complex_word | complex_list)
+
+    phrase = ZeroOrMore(expression)
+    phrase.setParseAction(process_emptys_and_singles)
+    phrase.setName('phrase')
 
     phrase.create_diagram('tcl_diag.html')
 
@@ -62,6 +92,34 @@ def test_quoted_word_list(parser):
         assert len(parsed) == 2
         assert list(parsed) == value.strip('"').split()
 
+
+def test_string_and_quoted_word_list(parser):
+    tests_and_expecteds = {
+        '"abc" "123 456"': ['abc', ['123', '456']],
+        '"" "123 456"':    ['', ['123', '456']],
+    }
+
+    for test, expected in tests_and_expecteds.items():
+        parsed = parser.parseString(test)
+
+
+        assert list(parsed.asList()) == expected
+
+
+def test_complex_list(parser):
+    tests_and_expecteds = {
+        '{123 456}':          ['123', '456'],
+        '"" {123 456}':       ['', ['123', '456']],
+        '{} {123 456}':       ['', ['123', '456']],
+        '{} {{} 123 456}':    ['', [[], '123', '456']],
+        '{} {{} 123 {} 456}': ['', [[], '123', [], '456']],
+        '{} "{} 123 {} 456"': ['', [[], '123', [], '456']],
+    }
+
+    for test, expected in tests_and_expecteds.items():
+        parsed = parser.parseString(test)
+
+        assert list(parsed.asList()) == expected
 
 if __name__ == '__main__':
     pytest.main([__file__, '-vv'])
