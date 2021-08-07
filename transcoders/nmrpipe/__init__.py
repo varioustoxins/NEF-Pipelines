579d9de2d5bfa922cc5b80110411c193023d8ce6
varioustoxins <garyt.and.sarahb@gmail.com>
2021-08-07 22:16:29 +0100

    add initialiser for nmrpipe module

---
+++ transcoders/nmrpipe/__init__.py
@@ -0,0 +1,13 @@
+import typer
+import nef_app
+app = typer.Typer()
+import_app = typer.Typer()
+
+if nef_app.app:
+    nef_app.app.add_typer(app, name='nmrpipe', help='-  read nmrpipe [peaks & sequencess]')
+
+    app.add_typer(import_app, name='import', help='-  import nmrpipe [peaks, & sequences]')
+
+    # import of specific importers must be after app creation to avoid circular imports
+    import transcoders.nmrpipe.importers.sequence
+    import transcoders.nmrpipe.importers.peaks
