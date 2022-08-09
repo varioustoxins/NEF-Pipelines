14d1ea0ee56e8380e6ad582b40be141401637bcf
varioustoxins <garyt.and.sarahb@gmail.com>
2022-08-09 10:09:21 +0100

    nef to pandas and pandas to nef now support round tripping or resetting the category...

--- lib/nef_lib.py
+++ lib/nef_lib.py
@@ -3,9 +3,22 @@ from types import MappingProxyType
 from pandas import DataFrame
 from pynmrstar import Loop
 
+NEF_CATEGORY_ATTR = '__NEF_CATEGORY__'
+
+
 def loop_to_dataframe(loop: Loop) -> DataFrame:
+    """
+    convert a pynmrstar Loop to a pandas DataFrame. Note the Loop category is
+    saved in the dataframe's attrs['__NEF_CATEGORY__']
+
+    :param loop: the pynmrstar Loop
+    :return: a pandas DataFrame
+    """
     tags = loop.tags
     data = DataFrame()
+
+    # note this strips the preceding _
+    data.attrs[NEF_CATEGORY_ATTR] = loop.category[1:]
 
     for tag in tags:
         if tag!= 'index':
@@ -14,6 +27,13 @@ def loop_to_dataframe(loop: Loop) -> DataFrame:
     return data
 
 def dataframe_to_loop(frame: DataFrame, category: str =  None) -> Loop:
+    """
+    convert a pandas DataFrame to a pynmrstar Loop
+
+    :param frame: the pandas DataFrame
+    :param category: the star category note this will override any category stored in attrs
+    :return: the new pynmrstar Loop
+    """
     loop = Loop.from_scratch(category=category)
     loop_data = {}
     for column in frame.columns:
@@ -21,6 +41,11 @@ def dataframe_to_loop(frame: DataFrame, category: str =  None) -> Loop:
         loop_data[column] = list(frame[column])
 
     loop.add_data(loop_data)
+
+    if NEF_CATEGORY_ATTR in frame.attrs and not category:
+        loop.set_category(frame.attrs[NEF_CATEGORY_ATTR])
+    elif category:
+        loop.set_category(category)
 
     return loop
 
--- tests/test_nef_lib.py
+++ tests/test_nef_lib.py
@@ -1,7 +1,7 @@
 from pandas import DataFrame
 from pynmrstar import Loop
 
-from lib.nef_lib import loop_to_dataframe, dataframe_to_loop
+from lib.nef_lib import loop_to_dataframe, dataframe_to_loop, NEF_CATEGORY_ATTR
 
 
 def test_nef_to_pandas():
@@ -44,3 +44,30 @@ def test_pandas_to_nef():
     result = dataframe_to_loop(data_frame, category='test_loop')
 
     assert result == EXPECTED_NEF
+
+def test_nef_category():
+
+    TEST_DATA_NEF = '''
+        loop_
+          _test_loop.tag_1 _test_loop.tag_2
+          1                 2
+          3                 .
+        stop_
+    '''
+
+    loop = Loop.from_string(TEST_DATA_NEF, convert_data_types=True)
+    frame = loop_to_dataframe(loop)
+
+    assert frame.attrs[NEF_CATEGORY_ATTR] == 'test_loop'
+
+    new_loop = dataframe_to_loop(frame)
+
+    # note pynmrstar includes the leading _ in the category, I don't...!
+    assert new_loop.category == '_test_loop'
+
+    new_loop_2 = dataframe_to_loop(frame, category='wibble')
+    # note pynmrstar includes the leading _ in the category, I don't...!
+    assert new_loop_2.category == '_wibble'
+
+
+
