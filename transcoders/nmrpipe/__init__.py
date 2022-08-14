2a64f5451112a2defe00200d64b043fb118a340e
varioustoxins <garyt.and.sarahb@gmail.com>
2022-08-14 22:24:35 +0100

    note nmrpipe can now read shifts as well in help string

--- transcoders/nmrpipe/__init__.py
+++ transcoders/nmrpipe/__init__.py
@@ -4,9 +4,9 @@ app = typer.Typer()
 import_app = typer.Typer()
 
 if nef_app.app:
-    nef_app.app.add_typer(app, name='nmrpipe', help='-  read nmrpipe [peaks & sequencess]')
+    nef_app.app.add_typer(app, name='nmrpipe', help='-  read nmrpipe [peaks shifts & sequencess]')
 
-    app.add_typer(import_app, name='import', help='-  import nmrpipe [peaks, & sequences]')
+    app.add_typer(import_app, name='import', help='-  import nmrpipe [peaks, shifts & sequences]')
 
     # import of specific importers must be after app creation to avoid circular imports
     import transcoders.nmrpipe.importers.sequence
