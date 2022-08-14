575186eca59a07df0c7ac76f533df05a8ba77849
varioustoxins <garyt.and.sarahb@gmail.com>
2022-08-14 22:20:52 +0100

    add ability to select frames rename is carried out on by name or category

--- tools/chains/rename.py
+++ tools/chains/rename.py
@@ -20,9 +20,10 @@ app = typer.Typer()
 def rename(
     old: str = Argument(...,  help='old chain code'),
     new: str = Argument(..., help='new chain code'),
-    comment: bool = Option(False, '-c', '--comment', help='prepend comment to chains'),
+    comment: bool = Option(False, '--comment', help='prepend comment to chains'),
     verbose: bool = Option(False, '-v', '--verbose', help='print verbose info'),
-    frames: List[str] = Option([], '-f', '--frame', help='limit changes to a a particular frame'),
+    use_category: bool = Option(False, '-c', '--category', help='select frames to rename chains in by category rather than name'),
+    frames: List[str] = Option([], '-f', '--frame', help='limit changes to a a particular frame by name [or category if --category is set], note: wildcards are allowed, repeated uses add more frames'),
 
 ):
     """- change the name of chains across one or multuiple frames"""
@@ -33,22 +34,30 @@ def rename(
     changes = 0
     changed_frames = OrderedSet()
     for save_frame in entry:
-        if len(frames) >= 1:
-            for frame_selector in frames:
-                if not fnmatch(save_frame.name, f'*{frame_selector}*'):
-                    continue
-        for loop in save_frame.loop_iterator():
-            for tag in loop.get_tag_names():
-                tag_parts = tag.split('.')
-                if tag_parts[-1].startswith('chain_code'):
-                    tag_values = loop[tag]
-                    for i, row in enumerate(tag_values):
-                        if row == old:
-                            tag_values[i] = new
-                            changes += 1
-                            changed_frames.add(save_frame.name)
+        process_frame =  True
+        for frame_selector in frames:
 
-                    loop[tag] = tag_values
+            if use_category:
+                if not fnmatch(save_frame.category, f'*{frame_selector}*'):
+
+                    process_frame = False
+            else:
+                if not fnmatch(save_frame.name, f'*{frame_selector}*'):
+                    process_frame = False
+
+        if process_frame:
+            for loop in save_frame.loop_iterator():
+                for tag in loop.get_tag_names():
+                    tag_parts = tag.split('.')
+                    if tag_parts[-1].startswith('chain_code'):
+                        tag_values = loop[tag]
+                        for i, row in enumerate(tag_values):
+                            if row == old:
+                                tag_values[i] = new
+                                changes += 1
+                                changed_frames.add(save_frame.name)
+
+                        loop[tag] = tag_values
 
     if verbose:
         comment = '# ' if comment else ''
