f2ccd6be6a873b9b4979304fbb7b2b9f936694a2
varioustoxins <garyt.and.sarahb@gmail.com>
2022-12-15 20:40:15 +0000

    clear_cache sometimes accessing gc'd objects

--- src/nef_pipelines/tests/conftest.py
+++ src/nef_pipelines/tests/conftest.py
@@ -18,9 +18,13 @@ def clear_cache():
     import gc
 
     gc.collect()
-    wrappers = [
-        a for a in gc.get_objects() if isinstance(a, functools._lru_cache_wrapper)
-    ]
+    wrappers = []
+    for elem in gc.get_objects():
+        try:
+            if isinstance(elem, functools._lru_cache_wrapper):
+                wrappers.append(elem)
+        except ReferenceError:
+            pass
 
     for wrapper in wrappers:
         wrapper.cache_clear()
