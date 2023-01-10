bc35a520ad1f74a2a66410da653bb0034f96c758
varioustoxins <garyt.and.sarahb@gmail.com>
2023-01-10 13:24:01 +0000

    update test to new standards

--- src/nef_pipelines/tests/nmrpipe/test_shifts.py
+++ src/nef_pipelines/tests/nmrpipe/test_shifts.py
@@ -72,41 +72,39 @@ def test_lib_parse_shifts():
     assert shifts == EXPECTED
 
 
-EXPECTED_NS3_S135A_BMRB1_SHORT = """\
-    save_nef_chemical_shift_list_nmrpipe
-       _nef_chemical_shift_list.sf_category   nef_chemical_shift_list
-       _nef_chemical_shift_list.sf_framecode  nef_chemical_shift_list_nmrpipe
-
-       loop_
-          _nef_chemical_shift_list.chain_code
-          _nef_chemical_shift_list.sequence_code
-          _nef_chemical_shift_list.residue_name
-          _nef_chemical_shift_list.atom_name
-          _nef_chemical_shift_list.value
-          _nef_chemical_shift_list.value_uncertainty
-          _nef_chemical_shift_list.element
-          _nef_chemical_shift_list.isotope_number
-
-         A   1   ALA   N     125.74   .   .   .
-         A   1   ALA   C     179.08   .   .   .
-         A   2   GLY   CA    44.77    .   .   .
-         A   2   GLY   HA2   3.89     .   .   .
-         A   2   GLY   HA3   3.8      .   .   .
-
-       stop_
-
-    save_
-"""
-
-
 # noinspection PyUnusedLocal
 def test_ns3_S135A_BMRB1_short():
+    EXPECTED = """\
+        save_nef_chemical_shift_list_nmrpipe
+           _nef_chemical_shift_list.sf_category   nef_chemical_shift_list
+           _nef_chemical_shift_list.sf_framecode  nef_chemical_shift_list_nmrpipe
 
+           loop_
+              _nef_chemical_shift_list.chain_code
+              _nef_chemical_shift_list.sequence_code
+              _nef_chemical_shift_list.residue_name
+              _nef_chemical_shift_list.atom_name
+              _nef_chemical_shift_list.value
+              _nef_chemical_shift_list.value_uncertainty
+              _nef_chemical_shift_list.element
+              _nef_chemical_shift_list.isotope_number
+
+             A   1   ALA   N     125.74   .   .   .
+             A   1   ALA   C     179.08   .   .   .
+             A   2   GLY   CA    44.77    .   .   .
+             A   2   GLY   HA2   3.89     .   .   .
+             A   2   GLY   HA3   3.8      .   .   .
+
+           stop_
+
+        save_
+    """
     path = path_in_test_data(__file__, "ns3_S135A_BMRB1_short.txt")
     result = run_and_report(app, [path], input=HEADER)
 
-    if result.exit_code != 0:
-        print(result.stdout)
+    mol_sys_result = isolate_frame(result.stdout, SHIFTS_NMRPIPE)
+
+    assert_lines_match(EXPECTED, mol_sys_result)
 
     assert result.exit_code == 0
     mol_sys_result = isolate_frame(result.stdout, "%s" % SHIFTS_NMRPIPE)
