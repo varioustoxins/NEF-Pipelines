784ccd564aa70b52c8b2f5f3ceffbde2448e2448
varioustoxins <garyt.and.sarahb@gmail.com>
2023-03-19 00:15:09 +0000

    remove sparky import peaks import for un committed module

--- src/nef_pipelines/transcoders/sparky/__init__.py
+++ src/nef_pipelines/transcoders/sparky/__init__.py
@@ -20,5 +20,4 @@ if nef_app.app:
 
     # import of specific importers must be after app creation to avoid circular imports
     import nef_pipelines.transcoders.sparky.exporters.peaks  # noqa: F401
-    import nef_pipelines.transcoders.sparky.importers.peaks  # noqa: F401
     import nef_pipelines.transcoders.sparky.importers.shifts  # noqa: F401
