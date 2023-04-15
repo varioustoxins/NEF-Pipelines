2abcb19bfb6bf3f77e1341881f55376d893d282f
varioustoxins <garyt.and.sarahb@gmail.com>
2023-04-15 17:42:43 +0100

    correct errors in release to pypi script:
    - git dirty check didn't work
    - git stash check wasn't checking it it stashed files before popping

--- release_to_pypi.sh
+++ release_to_pypi.sh
@@ -2,7 +2,13 @@
 git update-index --refresh >/dev/null
 git diff-index --quiet HEAD --
 
-test $? -eq 0 || echo "ERROR: the repo is dirty clean it before building!"; exit 1
+
+if [[ $? -ne 0 ]]; then
+	echo "ERROR: the repo is dirty clean it before building!"
+    exit 1
+fi
+
+echo got here
 
 # define where the version file is found
 VERSION_FILE=src/nef_pipelines/VERSION
@@ -28,11 +34,15 @@ git commit -m "updated version to $VERSION" --no-verify
 ver  --file $VERSION_FILE tag
 
 # stash any uncommited changes to avoid version+1 errors
-git stash
+stash_message="stashed-for-pypi-drop-${VERSION}-`date +%s`"
+git stash --message ${stash_message}
 
 #build and publish
 tox -e build  # to build your package distribution
 tox -e publish -- --repository pypi
 
-# unstash uncommitted changes
-git stash pop
+git stash list  | grep -q ${stash_message}
+if [[ $? -eq 0 ]]; then
+  # unstash uncommitted changes
+  git stash pop
+fi
