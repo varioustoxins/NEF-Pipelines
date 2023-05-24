bfd51c1ca82b00b6e0ec7db6f1d547507752ef72
varioustoxins <garyt.and.sarahb@gmail.com>
2023-05-24 22:29:16 +0100

    correct typos improve structure of code and used renamed library functions

--- src/nef_pipelines/transcoders/mars/exporters/fragments.py
+++ src/nef_pipelines/transcoders/mars/exporters/fragments.py
@@ -2,7 +2,7 @@
 import sys
 from pathlib import Path
 from string import digits
-from typing import Iterable, Iterator, List, Tuple
+from typing import List, Tuple
 
 import typer
 from pynmrstar import Entry, Saveframe
@@ -13,7 +13,13 @@ from nef_pipelines.lib.nef_lib import (
     read_entry_from_file_or_stdin_or_exit_error,
 )
 from nef_pipelines.lib.structures import LineInfo
-from nef_pipelines.lib.util import STDIN, end_with_ordinal, exit_error
+from nef_pipelines.lib.util import (
+    STDIN,
+    end_with_ordinal,
+    exit_error,
+    iter_consecutive_pairs,
+    remove_duplicates_stable,
+)
 from nef_pipelines.transcoders.mars import export_app
 
 NMR_RESIDUE = "nmr_residue"
@@ -52,7 +58,7 @@ def fragments(
         metavar="<MARS_SHIFT_FILE>",
     ),
 ):
-    """- convert nef chemical shifts to mars"""
+    """- convert nef chemical shifts to mars fragments"""
 
     entry = read_entry_from_file_or_stdin_or_exit_error(input)
 
@@ -70,20 +76,15 @@ def fragments(
     if output_file != STDOUT_PATH:
         file_h.close()
 
-    if output_file != STDOUT_PATH:
-        if not sys.stdout.isatty():
-            print(entry)
+    if output_file != STDOUT_PATH and not sys.stdout.isatty():
+        print(entry)
 
 
 def _format_table(table: List[List[Tuple[str, str]]]) -> List[List[str]]:
     max_overall_elem_length = 0
     for chain in table:
-        max_elem_length = max([len(" ".join(prs)) for prs in chain])
-        max_overall_elem_length = (
-            max_elem_length
-            if max_elem_length > max_overall_elem_length
-            else max_overall_elem_length
-        )
+        max_elem_length = max(len(" ".join(prs)) for prs in chain)
+        max_overall_elem_length = max(max_elem_length, max_overall_elem_length)
     for chain in table:
         for i, prs in enumerate(chain):
             pr_length = len(prs[0]) + len(prs[1])
@@ -105,7 +106,7 @@ def _build_connected_pseudo_residues(connected_residues):
     for chain_code, residue_codes in connected_residues.items():
         chain = [
             (f"PR_{first}", f"PR_{second}")
-            for first, second in overlapped_pairs(residue_codes)
+            for first, second in iter_consecutive_pairs(residue_codes)
         ]
         ends.append(f"#{chain_code}")
         table.append(chain)
@@ -114,10 +115,11 @@ def _build_connected_pseudo_residues(connected_residues):
 
 
 def _get_connected_residues_and_chains(frame: Saveframe, entry: Entry):
-    connected_chains = set()
-    for chain in loop_row_namespace_iter(frame.get_loop(NMR_CHAIN)):
-        if chain.is_connected:
-            connected_chains.add(chain.short_name)
+    connected_chains = {
+        chain.short_name
+        for chain in loop_row_namespace_iter(frame.get_loop(NMR_CHAIN))
+        if chain.is_connected
+    }
     connected_residues = {}
     nmr_residue_loop = frame.get_loop(NMR_RESIDUE)
     for line_no, residue in enumerate(
@@ -148,8 +150,7 @@ def _get_assignment_frame_or_exit(entry):
                     there names were {' '.join([frame.name for frame in frames])}
             """
         exit_error(msg)
-    frame = frames[0]
-    return frame
+    return frames[0]
 
 
 def _get_connected_residue_or_exit_error(residue, line_info: LineInfo):
@@ -169,20 +170,6 @@ def _get_connected_residue_or_exit_error(residue, line_info: LineInfo):
     return sequence_code
 
 
-# https://stackoverflow.com/questions/480214/how-do-i-remove-duplicates-from-a-list-while-preserving-order
-def remove_duplicates_stable(seq: Iterable) -> List:
-
-    seen = set()
-    seen_add = seen.add
-    return [x for x in seq if not (x in seen or seen_add(x))]
-
-
-# https://stackoverflow.com/questions/21303224/iterate-over-all-pairs-of-consecutive-items-in-a-list
-def overlapped_pairs(seq: Iterator) -> Iterator[Tuple]:
-    for first, second in zip(seq, seq[1:]):
-        yield first, second
-
-
 def _filter_headings_by_pseudoatoms(base_headers, pseudo_residues):
     atom_names = set()
     for pseudo_residue in pseudo_residues.values():
@@ -193,8 +180,4 @@ def _filter_headings_by_pseudoatoms(base_headers, pseudo_residues):
                 else ""
             )
             atom_names.add(f"{pseudo_atom.atom_name.upper()}{offset}")
-    headers = []
-    for header in base_headers:
-        if header == "" or header in atom_names:
-            headers.append(header)
-    return headers
+    return [header for header in base_headers if header == "" or header in atom_names]
