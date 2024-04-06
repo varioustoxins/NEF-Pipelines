3b8db9a3f0d806b94a477f7bbd43c9a48f1d8504
varioustoxins <garyt.and.sarahb@gmail.com>
2024-04-06 17:50:46 +0100

    add missing init file  for peaks package

---
+++ src/nef_pipelines/tools/peaks/__init__.py
@@ -0,0 +1,16 @@
+import typer
+
+from nef_pipelines import nef_app
+
+peaks_app = typer.Typer()
+
+
+if nef_app.app:
+    nef_app.app.add_typer(
+        peaks_app,
+        name="peaks",
+        help="-  carry out operations on nef peaks",
+    )
+
+    # import of specific importers must be after app creation to avoid circular imports
+    import nef_pipelines.tools.peaks.match  # noqa: F401
