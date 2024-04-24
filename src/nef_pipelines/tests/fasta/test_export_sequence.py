4a9838d8f0e856053240fcfd5729067fcd55fb0f
varioustoxins <garyt.and.sarahb@gmail.com>
2024-04-24 22:14:20 +0100

    add tests for exporting fasta sequences

---
+++ src/nef_pipelines/tests/fasta/test_export_sequence.py
@@ -0,0 +1,59 @@
+import typer
+from typer.testing import CliRunner
+
+from nef_pipelines.lib.test_lib import (
+    assert_lines_match,
+    path_in_test_data,
+    run_and_report,
+)
+from nef_pipelines.transcoders.fasta.exporters.sequence import sequence
+
+runner = CliRunner()
+app = typer.Typer()
+app.command()(sequence)
+
+EXPECTED_3AA = """
+>CHAIN: A | START RESIDUE: 1
+
+AAA
+"""
+
+
+# noinspection PyUnusedLocal
+def test_3aa():
+
+    path = path_in_test_data(__file__, "3aa.nef")
+    result = run_and_report(app, ["--in", path, "-"])
+
+    assert_lines_match(result.stdout, EXPECTED_3AA)
+
+
+EXPECTED_3AA_2_CHAINS = """
+>CHAIN: A | START RESIDUE: 1
+
+AAA
+>CHAIN: B | START RESIDUE: 1
+
+CCC
+"""
+
+
+def test_3aa_2_chains():
+
+    path = path_in_test_data(__file__, "3aa_2_chains.nef")
+    result = run_and_report(app, ["--in", path, "-"])
+
+    assert_lines_match(result.stdout, EXPECTED_3AA_2_CHAINS)
+
+
+EXPECTED_THX = """
+>CHAIN: thx | START RESIDUE: 1
+AAA
+"""
+
+
+def test_petes_thx_chain():
+    path = path_in_test_data(__file__, "3aa_thx.nef")
+    result = run_and_report(app, ["--in", path, "-"])
+
+    assert_lines_match(result.stdout, EXPECTED_THX)
