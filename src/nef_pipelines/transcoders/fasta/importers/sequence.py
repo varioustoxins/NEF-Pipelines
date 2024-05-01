e52272e2a414733b2e66302924864f08bc13a3df
varioustoxins <garyt.and.sarahb@gmail.com>
2024-05-01 18:52:22 +0100

    numerous internal improvements, molecule type applys per sequence, provides a pipe function etc

--- src/nef_pipelines/transcoders/fasta/importers/sequence.py
+++ src/nef_pipelines/transcoders/fasta/importers/sequence.py
@@ -1,15 +1,19 @@
-from itertools import chain, cycle, islice, zip_longest
+from itertools import chain, cycle, islice
 from pathlib import Path
 from typing import Iterable, List
 
 import typer
 from fastaparser import Reader
 from ordered_set import OrderedSet
+from pynmrstar import Entry
 
+from nef_pipelines.lib.nef_lib import (
+    add_frames_to_entry,
+    read_or_create_entry_exit_error_on_bad_file,
+)
 from nef_pipelines.lib.sequence_lib import (
     BadResidue,
     MoleculeType,
-    MoleculeTypes,
     chain_code_iter,
     offset_chain_residues,
     sequence_3let_to_res,
@@ -17,13 +21,7 @@ from nef_pipelines.lib.sequence_lib import (
     translate_1_to_3,
 )
 from nef_pipelines.lib.structures import SequenceResidue
-from nef_pipelines.lib.typer_utils import get_args
-from nef_pipelines.lib.util import (
-    STDIN,
-    exit_error,
-    parse_comma_separated_options,
-    process_stream_and_add_frames,
-)
+from nef_pipelines.lib.util import STDIN, exit_error, parse_comma_separated_options
 from nef_pipelines.transcoders.fasta import import_app
 
 app = typer.Typer()
@@ -60,11 +58,11 @@ def sequence(
     ),
     # TODO: unused inputs!
     entry_name: str = typer.Option("fasta", help="a name for the entry if required"),
-    input: Path = typer.Option(
+    in_file: Path = typer.Option(
         STDIN,
         "-i",
         "--in",
-        help="pipe to read NEF data from, for testing [overrides stdin !use stdin instead!]",
+        help="input to read NEF data from [- is stdin]",
     ),
     molecule_types: List[MoleculeType] = typer.Option(
         [
@@ -79,21 +77,31 @@ def sequence(
 ):
     """- convert fasta sequence to nef"""
 
+    file_names = parse_comma_separated_options(file_names)
+
     chain_codes = parse_comma_separated_options(chain_codes)
     if not chain_codes:
-        chain_codes = ["A"]
+        chain_codes = [
+            "A",
+        ]
 
     no_chain_starts = parse_comma_separated_options(no_chain_starts)
     if not no_chain_starts:
-        no_chain_starts = [False]
+        no_chain_starts = [
+            False,
+        ]
 
     no_chain_ends = parse_comma_separated_options(no_chain_ends)
     if no_chain_ends:
-        no_chain_ends = [False]
+        no_chain_ends = [
+            False,
+        ]
 
     starts = [int(elem) for elem in parse_comma_separated_options(starts)]
     if not starts:
-        starts = [1]
+        starts = [
+            1,
+        ]
 
     molecule_types = parse_comma_separated_options(molecule_types)
     if not molecule_types:
@@ -101,31 +109,44 @@ def sequence(
             MoleculeType.PROTEIN,
         ]
 
-    args = get_args()
+    entry = read_or_create_entry_exit_error_on_bad_file(in_file)
 
-    args.chain_codes = chain_codes
-    args.no_chain_starts = no_chain_starts
-    args.no_chain_ends = no_chain_ends
-    args.starts = starts
-    args.molecule_types = molecule_types
+    entry = pipe(
+        entry,
+        chain_codes,
+        starts,
+        no_chain_starts,
+        no_chain_ends,
+        molecule_types,
+        file_names,
+        in_file,
+    )
 
-    # TODO: doesn't provide a pipe command
-    process_sequences(args)
+    print(entry)
 
 
-def process_sequences(args):
+def pipe(
+    entry: Entry,
+    chain_codes: List[str],
+    starts: List[int],
+    no_chain_starts: List[bool],
+    no_chain_ends: List[bool],
+    molecule_types: List[MoleculeType],
+    file_names,
+    input: Path,
+):
     fasta_frames = []
 
-    chain_code_iterator = chain_code_iter(args.chain_codes)
+    chain_code_iterator = chain_code_iter(chain_codes)
     fasta_sequences = OrderedSet()
-    for file_path in args.file_names:
-        fasta_sequences.update(
-            read_sequences(file_path, chain_code_iterator, args.molecule_types)
-        )
+
+    fasta_sequences.update(
+        _read_sequences(file_names, chain_code_iterator, molecule_types)
+    )
 
     read_chain_codes = residues_to_chain_codes(fasta_sequences)
 
-    offsets = _get_sequence_offsets(read_chain_codes, args.starts)
+    offsets = _get_sequence_offsets(read_chain_codes, starts)
 
     fasta_sequences = offset_chain_residues(fasta_sequences, offsets)
 
@@ -133,9 +154,9 @@ def process_sequences(args):
 
     fasta_frames.append(sequence_to_nef_frame(fasta_sequences))
 
-    entry = process_stream_and_add_frames(fasta_frames, args)
+    entry = add_frames_to_entry(entry, fasta_frames)
 
-    print(entry)
+    return entry
 
 
 def _get_sequence_offsets(chain_codes: List[str], starts: List[int]):
@@ -160,48 +181,56 @@ def residues_to_chain_codes(residues: List[SequenceResidue]) -> List[str]:
 
 # could do with taking a list of offsets
 # noinspection PyUnusedLocal
-def read_sequences(
-    path: Path, chain_codes: Iterable[str], molecule_types: List[MoleculeType]
+def _read_sequences(
+    file_paths: List[Path],
+    chain_codes: Iterable[str],
+    molecule_types: List[MoleculeType],
 ) -> List[SequenceResidue]:
 
-    residues = OrderedSet()
-    try:
-        with open(path) as handle:
-            try:
-                reader = Reader(handle)
-                sequences = [sequence for sequence in reader]
-            except Exception as e:
-                # check if relative to os.getcwd
-                exit_error(f"Error reading fasta file {str(path)}", e)
-
-            number_sequences = len(sequences)
-
-            # read as many chain codes as there are sequences
-            # https://stackoverflow.com/questions/16188270/get-a-fixed-number-of-items-from-a-generator
-
-            chain_codes = list(islice(chain_codes, number_sequences))
-
-            for sequence, chain_code, molecule_type in zip_longest(
-                sequences, chain_codes, molecule_types, fillvalue=None
-            ):
-                if molecule_type is None:
-                    molecule_type = MoleculeTypes.PROTEIN
-
-                sequence = [letter_code.letter_code for letter_code in sequence]
+    sequences = []
+    headers = []
+    for file_path in file_paths:
+        try:
+            with open(file_path) as handle:
                 try:
-                    sequence_3_let = translate_1_to_3(
-                        sequence, molecule_type=molecule_type
-                    )
-                except BadResidue as e:
-                    exit_error(
-                        f"Error translating sequence {sequence} to 3 letter code {e}",
-                        e,
-                    )
-                chain_residues = sequence_3let_to_res(sequence_3_let, chain_code)
+                    reader = Reader(handle)
+                    sequences.extend([sequence for sequence in reader])
+                    headers.extend([header for header in reader])
+                except Exception as e:
+                    # check if relative to os.getcwd
+                    exit_error(f"Error reading fasta file {str(file_path)}", e)
+        except IOError as e:
+            exit_error(f"couldn't open {file_path} because:\n{e}", e)
 
-                residues.update(chain_residues)
+    number_sequences = len(sequences)
+    number_molecule_types = len(molecule_types)
 
-    except IOError as e:
-        exit_error(f"couldn't open {path} because:\n{e}", e)
+    if number_molecule_types == 1:
+        molecule_types = molecule_types * number_sequences
+    elif number_molecule_types == 0 or number_molecule_types < number_sequences:
+        msg = f"""
+            number molecule types [{number_molecule_types}] is different from number of chains {number_sequences}
+        """
+        exit_error(msg)
+
+    residues = OrderedSet()
+    # read as many chain codes as there are sequences
+    # https://stackoverflow.com/questions/16188270/get-a-fixed-number-of-items-from-a-generator
+    for sequence, chain_code, molecule_type in zip(
+        sequences, chain_code_iter(chain_codes), molecule_types
+    ):
+
+        sequence = [letter_code.letter_code for letter_code in sequence]
+
+        try:
+            sequence_3_let = translate_1_to_3(sequence, molecule_type=molecule_type)
+        except BadResidue as e:
+            exit_error(
+                f"Error translating sequence {sequence} to 3 letter code {e}",
+                e,
+            )
+        chain_residues = sequence_3let_to_res(sequence_3_let, chain_code)
+
+        residues.update(chain_residues)
 
     return residues
