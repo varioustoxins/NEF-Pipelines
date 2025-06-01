7086b568f64012632c6aed4368b802fc16a1b01b
varioustoxins <garyt.and.sarahb@gmail.com>
2025-06-01 19:04:13 +0100

    setup basic_install.sh for unattended installation

--- install_scripts/basic_install.sh
+++ install_scripts/basic_install.sh
@@ -1,3 +1,4 @@
 
+export NEF_PIPELINES_AUTO_INSTALL=yes
   curl -sL https://raw.githubusercontent.com/varioustoxins/NEF-Pipelines/refs/heads/master/install_scripts/install.sh \
-| bash
+| bash --yes
