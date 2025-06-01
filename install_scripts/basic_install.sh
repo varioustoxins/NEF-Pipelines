c044092cc352740e135a214c941a661229814c3c
varioustoxins <garyt.and.sarahb@gmail.com>
2025-06-01 19:06:14 +0100

    basic_install.sh remove incorrect --yes option

--- install_scripts/basic_install.sh
+++ install_scripts/basic_install.sh
@@ -1,4 +1,4 @@
 
 export NEF_PIPELINES_AUTO_INSTALL=yes
   curl -sL https://raw.githubusercontent.com/varioustoxins/NEF-Pipelines/refs/heads/master/install_scripts/install.sh \
-| bash --yes
+| bash
