327f9d11a93dad57ba7f182d703a64f18411be28
varioustoxins <garyt.and.sarahb@gmail.com>
2025-09-11 21:28:06 +0100

    fix bug: rcsb align and rcsb trim weren't added to the command list and so weren't available on the command line

--- src/nef_pipelines/transcoders/rcsb/__init__.py
+++ src/nef_pipelines/transcoders/rcsb/__init__.py
@@ -9,7 +9,10 @@ import_app = typer.Typer()
 if nef_app.app:
     nef_app.app.add_typer(app, name="rcsb", help="- read pdb/cif [sequences]")
 
-    app.add_typer(import_app, name="import", help="- import pdb/cif [sequences]")
+    app.add_typer(import_app, name="import", help=r"\- import pdb/cif [sequences]")
 
-    # import of specific importers must be after app creation to avoid circular imports
-    import nef_pipelines.transcoders.rcsb.importers.sequence  # noqa: F401
+
+# import of specific importers must be after app creation to avoid circular imports
+import nef_pipelines.transcoders.rcsb.align  # noqa: F401 E402
+import nef_pipelines.transcoders.rcsb.importers.sequence  # noqa: F401 E402
+import nef_pipelines.transcoders.rcsb.trim  # noqa: F401 E402
