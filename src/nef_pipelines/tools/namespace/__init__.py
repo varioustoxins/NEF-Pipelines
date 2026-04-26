4c3a1d072598c646a435daa4b81c87807abc10f8
varioustoxins <garyt.and.sarahb@gmail.com>
2026-04-26 21:35:18 +0100

    add namespace defined as a CLI command

--- src/nef_pipelines/tools/namespace/__init__.py
+++ src/nef_pipelines/tools/namespace/__init__.py
@@ -7,8 +7,9 @@ namespace_app = Typer()
 nef_app.app.add_typer(
     namespace_app,
     name="namespace",
-    help="- manipulate namespaces",
-    rich_help_panel="Data analysis & manipulation",
+    help="- display, filter, delete and rename namespaces [only display currently implemented]",
+    rich_help_panel="NEF manipulation",
 )
 
+from nef_pipelines.tools.namespace import defined  # noqa: F401, E402
 from nef_pipelines.tools.namespace import list  # noqa: F401, E402
